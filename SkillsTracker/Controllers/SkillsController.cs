a33883032d6b9576ecae7d89bd6d45f3161facfc
Sam <samtpomarico@gmail.com>
2020-10-29 20:22:44 -0500

    Studio complete, minus table reformat bonus

--- SkillsTracker/Controllers/SkillsController.cs
+++ SkillsTracker/Controllers/SkillsController.cs
@@ -3,52 +3,65 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.TagHelpers;
 
 namespace SkillsTracker
 {
     public class SkillsController : Controller
     {
         [HttpGet]
-        [Route("skills/")]
+        [Route("/skills")]
         public IActionResult Index()
         {
             string html = "<h1>Skills Tracker</h1>" +
                 "<h2>Coding skills to learn:</h2>" +
                 "<ol>" +
-                "<li>JavaScript</li>" +
-                "<li>C#</li>" +
-                "<li>SQL</li>" +
+                    "<li>JavaScript</li>" +
+                    "<li>C#</li>" +
+                    "<li>SQL</li>" +
                 "</ol>";
             return Content(html, "text/html");
         }
 
-        [HttpGet]
-        [Route("skills/form")]
-        public IActionResult SubmitForm()
+        [HttpGet("skills/form")]
+        public IActionResult RenderForm()
         {
-            string formString = "<form method='post' action='skills/form'>" +
-                "<label for='date'>Date:</label><br>" +
-                "<input type='date' label id='date' <br><br>" +
-                "<label for='javascript'>JavaScript:</label><br>" +
-                "<select name='skillLevel'>" +
-                "<option value='beginner'>Beginner</option>" +
-                "<option value='intermediate'>Intermediate</option>" +
-                "<option value='advanced'>Advanced</option>" +
-                "</select><br>" +
-                "<label for='c#'>C#:</label><br>" +
-                "<select name='skillLevel'>" +
-                "<option value='beginner'>Beginner</option>" +
-                "<option value='intermediate'>Intermediate</option>" +
-                "<option value='advanced'>Advanced</option>" +
-                "</select><br>" +
-                "<label for='sql'>SQL:</label><br>" +
-                "<select name='skillLevel'>" +
-                "<option value='beginner'>Beginner</option>" +
-                "<option value='intermediate'>Intermediate</option>" +
-                "<option value='advanced'>Advanced</option>" +
-                "</select>" +
-                "</form>";
-            return Content(formString, "text/html");
+                string formString = "<form method='post' action='/skills/form/submitted'>" +
+                    "<label for='date'>Date:</label><br>" +
+                    "<input type= 'date' name= 'date' <br><br>" +
+                    "<label for='javascript'>JavaScript:</label><br>" +
+                    "<select name= 'jsSkill'>" +
+                        "<option value= 'beginner' >Beginner</option>" +
+                        "<option value= 'intermediate' >Intermediate</option>" +
+                        "<option value= 'advanced' >Advanced</option>" +
+                    "</select><br>" +
+                    "<label for='c#'>C#:</label><br>" +
+                    "<select name='cSkill'>" +
+                        "<option value= 'beginner' >Beginner</option>" +
+                        "<option value= 'intermediate' >Intermediate</option>" +
+                        "<option value= 'advanced' >Advanced</option>" +
+                    "</select><br>" +
+                    "<label for='sql'>SQL:</label><br>" +
+                    "<select name='sqlSkill'>" +
+                        "<option value='beginner'>Beginner</option>" +
+                        "<option value='intermediate'>Intermediate</option>" +
+                        "<option value='advanced'>Advanced</option>" +
+                    "</select>" +
+                    "<input type='submit' value='Submit'/>" +
+                    "</form>";
+                return Content(formString, "text/html");
+        }
+
+        [HttpPost("/skills/form/submitted")]
+        public IActionResult SubmitForm(string date, string jsSkill, string cSkill, string sqlSkill)
+        {
+
+            string values = "<h1>" + date + "</h1>" +
+                "<ol>" +
+                "<li>JavaScript: " + jsSkill + "</li>" +
+                "<li>C#: " + cSkill + "</li>" +
+                "<li>SQL: " + sqlSkill + "</li>";
+            return Content(values, "text/html");
         }
     }
 }
